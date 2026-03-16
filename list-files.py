2218001593e0eba941f12632c30c350822735df5
BUSETTY <you@example.com>
2026-03-16 15:13:14 +0530

    Added for loop

--- list-files.py
+++ list-files.py
@@ -3,3 +3,10 @@ import os
 folders = input("Please provide the list of folders path:").split
 
 print(folders)
+for folder in folders:
+    if os.path.exists(folder):
+        print(f"Files in {folder}:")
+        for file in os.listdir(folder):
+            print(file)
+    else:
+        print(f"{folder} does not exist.") 
